e9862f7c145edaf5e2762c00d8f9d35d3025ba70
albert.sigman <albert.sigman@zenecosystems.com>
2025-10-14 10:00:03 -0400

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,3 @@
-/* export const HELLO_EMAIL = 'hello@theloveclubbridal.com' */
-export const HELLO_EMAIL = 'theloveclubbridal@gmail.com'
+export const HELLO_EMAIL = 'hello@theloveclubbridal.com'
 
 export const PHONE_NUMBER = '(734) 210-8644'
