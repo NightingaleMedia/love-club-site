c12bac26fdaa397b074c7684c23dcbd1d26b408a
albert.sigman <albert.sigman@zenecosystems.com>
2025-12-15 22:24:44 -0500

    add script

--- src/components/shared/scripts.tsx
+++ src/components/shared/scripts.tsx
@@ -1,44 +1,15 @@
 import React from 'react'
 import { Script } from 'gatsby'
+import { GoogleTag } from './scripts/GoogleTag'
+import { FacebookTag } from './scripts/FacebookTag'
+import { Gtag } from './scripts/Gtag'
 
 export const Scripts = () => {
   return (
     <>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-J7J7CWG628"
-      ></Script>
-      <Script
-        dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-J7J7CWG628');`,
-        }}
-      ></Script>
-
-      <Script
-        dangerouslySetInnerHTML={{
-          __html: `!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1220379756817932');
-fbq('track', 'PageView');`,
-        }}
-      ></Script>
-      <noscript>
-        <img
-          height="1"
-          width="1"
-          style={{ display: 'none' }}
-          src="https://www.facebook.com/tr?id=1220379756817932&ev=PageView&noscript=1"
-        />
-      </noscript>
+      <Gtag />
+      <GoogleTag />
+      <FacebookTag />
     </>
   )
 }
